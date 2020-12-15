fa4720071a34c329ba7670533c32a06a3769fe34
Andre Pham <Andre-Pham@users.noreply.github.com>
2020-12-16 08:17:33 +1100

    implemented square visrep

--- text_to_visrep.py
+++ text_to_visrep.py
@@ -1,5 +1,10 @@
 # Text to visual representation
 
+import math
+
+def is_square(num):
+    return num == math.isqrt(num)**2
+
 def num_to_bin_list(num):
     '''
     Converts a given integer to binary in the form of a list, with each
@@ -11,13 +16,13 @@ def num_to_bin_list(num):
     bin_list = [int(i) for i in binary_num[2:]]
     return bin_list
 
-def len_to_ten(bin_list):
+def len_to_dyn(bin_list, dyn):
     '''
-    Adds 0s as elements to the front of a list until the list has ten elements.
-    Returns the adjusted list.
+    Adds 0s as elements to the front of a list until the list has a dynamic
+    number of elements. Returns the adjusted list.
     '''
     # Add 0s to the front of the list until it has a length of ten
-    while len(bin_list) < 10:
+    while len(bin_list) < dyn:
         bin_list.insert(0, 0)
     # Return the list
     return bin_list
@@ -28,30 +33,55 @@ def generate_visrep(input_text):
     (nested lists) with 0s and 1s as elements. Returns the visual
     representation.
     '''
+    # Determine char-list length
+    num_reps = [ord(char.lower()) for char in input_text]
+    largest_num_rep = max(num_reps)
+    char_list_length = len(bin(largest_num_rep)) - 2
+
+    # Something
+    # Note, the first 6 digits of visrep are char_list_length
+    num_blocks = (char_list_length + 1)*len(input_text) + 6
+    new_block_count = 0
+    while is_square(num_blocks) == False:
+        new_block_count += 1
+        num_blocks += 1
+    new_block_addition = [0 for _ in range(new_block_count)]
+
     # Define the visual representation to be returned
-    visrep = []
+    visrep = len_to_dyn(
+        num_to_bin_list(char_list_length + 1),
+        6
+    )
 
     # Loop through every letter of the input text
     for order, letter in enumerate(input_text):
         # Generate binary-list form of the number representation of the
         # character
         num_representation = ord(letter.lower())
-        bin_letter_list = len_to_ten(num_to_bin_list(num_representation))
+        bin_letter_list = len_to_dyn(
+            num_to_bin_list(num_representation),
+            char_list_length
+        )
         # Define is_capital as 0 if the letter isn't capital, and 1 if it is
         is_capital = 0
         if letter.isupper():
             is_capital = 1
-        # Generate the binary-list form of the location of the character
-        bin_location_list = len_to_ten(num_to_bin_list(order))
         # Add the letter's visual representation to the final text's visual
         # representation
-        visrep.append(bin_letter_list + [is_capital] + bin_location_list)
+        visrep += bin_letter_list + [is_capital]
+
+    visrep += new_block_addition
+
+    visrep_nested = []
+    row_length = math.isqrt(num_blocks)
+    for i in range(0, num_blocks, row_length):
+        visrep_nested.append(visrep[i:i+row_length])
 
     # Return the visual representation
-    return visrep
+    return visrep_nested
 
 # Testing
 if __name__ == "__main__":
-    test = generate_visrep("test ")
+    test = generate_visrep("https://www.python.org/dev/peps/pep-0008/#code-lay-out")
     for row in test:
         print(row)
--- visrep_to_text.py
+++ visrep_to_text.py
@@ -5,7 +5,7 @@ from text_to_visrep import *
 
 def binlist_to_int(binlist):
     '''
-    Generates an integer from a list, which contains 0s and 1s to represent a
+    Generates an integer from a list which contains 0s and 1s to represent a
     binary value.
     '''
     # Convert the list to a string which represents the binary datatype
@@ -20,30 +20,26 @@ def decode_visrep(visrep):
     (nested lists) which olds 0s and 1s. Returns the string.
     '''
     # Defines the string to be returned as a list
-    decoded_text_as_list = []
+    decoded_text = ""
 
-    # Loops through every nested list in the visrep, which each represents
-    # a character
-    for letter_rep in visrep:
+    flat_visrep = [bit for row in visrep for bit in row]
+    char_list_len = binlist_to_int(flat_visrep[:6])
+    for i in range(6, len(flat_visrep)-6, char_list_len):
+        letter_rep = flat_visrep[i:i+char_list_len]
         # Decode the character as a string
-        letter = chr(binlist_to_int(letter_rep[0:10]))
+        letter = chr(binlist_to_int(letter_rep[:-1]))
         # Capitalise the letter if necessary
-        if letter_rep[10] == 1:
+        if letter_rep[-1] == 1:
             letter = letter.upper()
-        # Decode the location of the character
-        location_num_representation = binlist_to_int(letter_rep[12:22])
-        # Insert the character into its correct location in decoded_text_as_list
-        decoded_text_as_list.insert(location_num_representation, letter)
+        # Add the character to the final string
+        decoded_text += letter
 
     # Return decoded_text_as_list as a string, which is the final decoded string
-    return "".join(decoded_text_as_list)
-
-def test(test):
-    return test + " hello there"
+    return decoded_text
 
 # Testing
 if __name__ == "__main__":
-    test = generate_visrep("https://www.qr-code-generator.com/")
+    test = generate_visrep("https://www.python.org/dev/peps/pep-0008/#code-lay-out")
     for row in test:
         print(row)
     print(decode_visrep(test))
