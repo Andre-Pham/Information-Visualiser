c3af16f45b62af30641388fa0424a035b7cd67a6
Andre Pham <Andre-Pham@users.noreply.github.com>
2020-12-15 22:44:17 +1100

    Added comments and docstrings

--- text_to_visrep.py
+++ text_to_visrep.py
@@ -1,29 +1,56 @@
 # Text to visual representation
 
-#input_text = input("What would you like to convert to a visual representation?\n")
-
 def num_to_bin_list(num):
+    '''
+    Converts a given integer to binary in the form of a list, with each
+    element holding a bit. Returns the list.
+    '''
+    # Converts number to binary
     binary_num = bin(num)
+    # Converts binary to list representation and returns it
     bin_list = [int(i) for i in binary_num[2:]]
     return bin_list
 
 def len_to_ten(bin_list):
+    '''
+    Adds 0s as elements to the front of a list until the list has ten elements.
+    Returns the adjusted list.
+    '''
+    # Add 0s to the front of the list until it has a length of ten
     while len(bin_list) < 10:
         bin_list.insert(0, 0)
+    # Return the list
     return bin_list
 
 def generate_visrep(input_text):
+    '''
+    Generates the visual representation of a string in the form of a 2D matrix
+    (nested lists) with 0s and 1s as elements. Returns the visual
+    representation.
+    '''
+    # Define the visual representation to be returned
     visrep = []
+
+    # Loop through every letter of the input text
     for order, letter in enumerate(input_text):
+        # Generate binary-list form of the number representation of the
+        # character
         num_representation = ord(letter.lower())
         bin_letter_list = len_to_ten(num_to_bin_list(num_representation))
+        # Define is_capital as 0 if the letter isn't capital, and 1 if it is
         is_capital = 0
         if letter.isupper():
             is_capital = 1
+        # Generate the binary-list form of the location of the character
         bin_location_list = len_to_ten(num_to_bin_list(order))
+        # Add the letter's visual representation to the final text's visual
+        # representation
         visrep.append(bin_letter_list + [is_capital] + bin_location_list)
+
+    # Return the visual representation
     return visrep
 
+# Testing
 if __name__ == "__main__":
     test = generate_visrep("test ")
     for row in test:
--- visrep_to_text.py
+++ visrep_to_text.py
@@ -1,25 +1,38 @@
 # Visual representation to text
 
+# Import complimenting scripts
 from text_to_visrep import *
 
 def decode_visrep(visrep):
+    '''
+    Generates the string from its visual representation, which is a 2D matrix
+    (nested lists) which olds 0s and 1s. Returns the string.
+    '''
+    # Defines the string to be returned as a list
     decoded_text_as_list = []
+
+    # Loops through every nested list in the visrep, which each represents
+    # a character
     for letter_rep in visrep:
+        # Decode the character as a string
         letter_bin_representation = "0b" + "".join(list(map(str, letter_rep[0:10])))
         letter_num_representation = int(letter_bin_representation, 2)
         letter = chr(letter_num_representation)
-
+        # Capitalise the letter if necessary
         if letter_rep[10] == 1:
             letter = letter.upper()
-
+        # Decode the location of the character
         location_bin_representaiton = "0b" + "".join(list(map(str, letter_rep[12:22])))
         location_num_representation = int(location_bin_representaiton, 2)
-
+        # Insert the character into its correct location in decoded_text_as_list
         decoded_text_as_list.insert(location_num_representation, letter)
+
+    # Return decoded_text_as_list as a string, which is the final decoded string
     return "".join(decoded_text_as_list)
 
+# Testing
 if __name__ == "__main__":
-    test = generate_visrep("that's my girl wh]][[]]")
+    test = generate_visrep("This is a test statement.")
     for row in test:
         print(row)
     print(decode_visrep(test))
