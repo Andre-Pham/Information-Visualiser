3d7230c71d7c0343e8bfa02815f8f0cb5a0c0dc1
Andre Pham <Andre-Pham@users.noreply.github.com>
2020-12-20 13:37:35 +1100

    Began scanning code

---
+++ scan_visrep.py
@@ -0,0 +1,19 @@
+
+from PIL import Image
+
+img = Image.open("test.png")
+
+img_color = Image.Image.getdata(img).convert("P")
+for i in img_color:
+    if i != 205:
+        print(i)
+
+print(img.width)
+'''
+black = (0, 0, 0, 255)
+white = (255, 255, 255, 255)
+black = 0
+white = 225
+
+purple = 205
+'''
