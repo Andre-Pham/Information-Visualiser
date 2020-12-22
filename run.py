401eadd90411b9a5d9786a2a7787a78ae835cb3f
Andre Pham <Andre-Pham@users.noreply.github.com>
2020-12-22 11:46:45 +1100

    re-created user interface using a class structure

--- run.py
+++ run.py
@@ -13,59 +13,69 @@ TEXTBOX_COLOR = "#d0d0d0"
 BUTTON_COLOR = "#8a69f3"
 FONT = "System"
 
+class Interface:
+    def __init__(self, window, geometry, title, bg):
+        self.window = window
+        self.drawn_elements = []
+        self.live_text_box = None
+
+        window.geometry(geometry)
+        window.title(title)
+        window.configure(bg = bg)
+
+        self.text_to_visrep_interface = [
+            tk.Label(
+                window,
+                text = "Enter text:",
+                font = FONT,
+                bg = BACKGROUND_COLOR,
+                fg = TEXT_COLOR
+            ),
+            tk.Text(
+                window,
+                width = 50,
+                height = 3,
+                font = FONT,
+                relief = "flat",
+                bg = TEXTBOX_COLOR,
+                fg = TEXT_COLOR
+            ),
+            tk.Button(
+                window,
+                text = "Generate VISREP",
+                relief = "flat",
+                font = FONT,
+                fg = TEXT_COLOR_HIGHLIGHT,
+                bg = BUTTON_COLOR,
+                command = lambda: self.generate_visrep_button()
+            )
+        ]
+
+    def clear_all(self):
+        for elmt in self.drawn_elements:
+            elmt.pack_forget()
+
+    def draw_text_to_visrep(self):
+        self.clear_all()
+        self.drawn_elements = []
+
+        self.live_text_box = self.text_to_visrep_interface[1]
+
+        for elmt in self.text_to_visrep_interface:
+            elmt.pack()
+            self.drawn_elements.append(elmt)
+
+    # Reaction functions
+    def generate_visrep_button(self):
+        text_input = self.live_text_box.get('1.0', tk.END).strip("\n")
+        visrep = generate_visrep(text_input)
+        generate_visrep_png(visrep)
+
+
 # Set up window
 window = tk.Tk()
-window.geometry("450x150")
-window.title("VISREP")
-window.configure(bg = BACKGROUND_COLOR)
-
-# Reaction functions
-def generate_visrep_button():
-    text_input = text_box.get('1.0', tk.END).strip("\n")
-    visrep = generate_visrep(text_input)
-    generate_visrep_png(visrep)
-
-# Declare UI features
-text_box = tk.Text(
-    window,
-    width = 50,
-    height = 3,
-    font = FONT,
-    relief = "flat",
-    bg = TEXTBOX_COLOR,
-    fg = TEXT_COLOR
-)
-
-elements = [
-    tk.Label(
-        window,
-        text = "Enter text:",
-        font = FONT,
-        bg = BACKGROUND_COLOR,
-        fg = TEXT_COLOR
-    ),
-    text_box,
-    tk.Button(
-        window,
-        text = "Generate VISREP",
-        relief = "flat",
-        font = FONT,
-        fg = TEXT_COLOR_HIGHLIGHT,
-        bg = BUTTON_COLOR,
-        command = generate_visrep_button
-    )
-]
-
-# Draw UI features
-for num, elmt in enumerate(elements):
-    elmt.pack()
-    '''
-    elmt.grid(
-        row = num,
-        column = 0,
-        sticky = "N"
-    )
-    '''
+interface = Interface(window, "450x150", "VISREP", BACKGROUND_COLOR)
+interface.draw_text_to_visrep()
 
 # Run window
 window.mainloop()
